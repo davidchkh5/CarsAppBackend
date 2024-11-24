b97378aac69bc4eeccefbfa85d5892be9db2bc2d
davidchkh5 <daviti.chkhaidze5@gmail.com>
2024-11-24 15:31:44 +0400

    Upgraded to .net9 + Bidservice started

---
+++ BiddingService/Controllers/BidsController.cs
@@ -0,0 +1,73 @@
+﻿using BiddingService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace BiddingService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BidsController : ControllerBase
+    {
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<Bid>> PlaceBid(string auctionId, int amount)
+        {
+            var auction = await DB.Find<Auction>().OneAsync(auctionId);
+
+            if(auction == null)
+            {
+                // TODO: check with auction service if that has auction
+                return NotFound();
+            }
+
+            if(auction.Seller == User.Identity.Name)
+            {
+                return BadRequest("You cannot bid on your auction");
+            }
+
+            var bid = new Bid
+            {
+                Amount = amount,
+                AuctionId = auctionId,
+                Bidder = User.Identity.Name
+            };
+
+            if(auction.AutionEnd < DateTime.UtcNow)
+            {
+                bid.BidStatus = BidStatus.Finished;
+            } else //Not Finished  
+            {
+
+                var highBid = await DB.Find<Bid>().Match(
+                    a => a.AuctionId == auctionId).Sort(b => b.Descending(x => x.Amount)).ExecuteFirstAsync();
+
+                if (highBid != null && amount > highBid.Amount || highBid == null)
+                {
+                    bid.BidStatus = amount > auction.ReservePrice ? BidStatus.Accepted : BidStatus.AcceptedBelowReserve;
+
+                }
+
+                if (highBid != null && bid.Amount <= highBid.Amount)
+                {
+                    bid.BidStatus = BidStatus.TooLow;
+                }
+            }
+            await DB.SaveAsync(bid);
+            return Ok(bid);
+        }
+
+
+
+        [HttpGet("{auctionId}")]
+        public async Task<ActionResult<List<Bid>>> GetBidsForAuction(string auctionId)
+        {
+            var bids = await DB.Find<Bid>().Match(a => a.AuctionId == auctionId)
+                .Sort(s => s.Descending(x => x.BidTime))
+                .ExecuteAsync();
+
+            return bids;
+        }
+    }
+}
